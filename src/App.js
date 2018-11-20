578b389793a53311639a75a949e8868d60bbcf61
Justin Castilla <justin.castilla@saucelabs.com>
2018-11-20 15:24:21 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Clipboard from 'react-clipboard.js';
+import { IconClipboard } from '@saucelabs/sl-component-icons';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { monokai } from 'react-syntax-highlighter/styles/hljs';
+import { javaCode } from './InstantSauceTest.js'
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      collapsed: true
+    }
+  }
+
+  toggleCollapse = () => {
+    this.setState({
+      collapsed: !this.state.collapsed 
+    })
+  }
+
   render() {
+
+    let codeBoxState = this.state.collapsed ? "collapsed" : "expanded";
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="codeBox">
+          <button onClick={ this.toggleCollapse }>View Full</button>
+          <Clipboard  data-clipboard-text={javaCode}>
+            {/* <IconClipboard
+              height="18"
+              width="18"
+            /> */}
+            Copy
+          </Clipboard>
+          <div className={codeBoxState}>
+            <SyntaxHighlighter language='java' showLineNumbers={true} style={ monokai}>{javaCode}</SyntaxHighlighter>
+          </div>
       </div>
     );
   }
